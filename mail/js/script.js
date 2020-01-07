98f7feac323b9c1071a45cbba0df82ed0cc0703c
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-07 15:36:37 +0100

    Update mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,10 +1,10 @@
 var emailUtentiRegistrati = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "minnie@gmail.com"];
-var trovato = false;
+
 
 document.getElementById('entra').addEventListener ("click",
 function(){
     var emailUtente = document.getElementById('email').value;
-
+    var trovato = false;
     // Controlla se la mail inserita nel form è uguale a una di quelle nell'Array
     // Se è uguale aggiorna la variabile "trovato"
     for (var i = 0; i < emailUtentiRegistrati.length; i++) {
